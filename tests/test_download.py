22df5c13d12cd930a81bdb3af2bf2358730f9f4f
Ryan Wolf <rywolf@nvidia.com>
2024-04-09 13:21:53 -0700

    Add test for imports
    Signed-off-by: Ryan Wolf <rywolf@nvidia.com>

---
+++ tests/test_download.py
@@ -0,0 +1,9 @@
+class TestDownload:
+    def test_imports():
+        from nemo_curator.download import (
+            download_arxiv,
+            download_common_crawl,
+            download_wikipedia,
+        )
+
+        assert True
