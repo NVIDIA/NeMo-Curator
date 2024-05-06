007b7d11852e1f5d3776ca103767f3782c4e5570
Miguel Martínez <26169771+miguelusque@users.noreply.github.com>
2024-05-06 23:20:46 +0200

    Add batched file reading
    In order to reduce potential OOMs, I have added support to batched files reading.
    
    Signed-off-by: Miguel Martínez <26169771+miguelusque@users.noreply.github.com>

--- nemo_curator/scripts/separate_by_metadata.py
+++ nemo_curator/scripts/separate_by_metadata.py
@@ -19,8 +19,8 @@ import shutil
 from nemo_curator.utils.distributed_utils import get_client, read_data
 from nemo_curator.utils.file_utils import (
     expand_outdir_and_mkdir,
-    get_all_files_paths_under,
     separate_by_metadata,
+    get_batched_files
 )
 from nemo_curator.utils.script_utils import add_distributed_args, attach_bool_arg
 
@@ -28,31 +28,40 @@ from nemo_curator.utils.script_utils import add_distributed_args, attach_bool_ar
 def main(args):
     client = get_client(args, args.device)
 
-    files = get_all_files_paths_under(args.input_data_dir)
-    input_data = read_data(
-        files, file_type=args.input_file_type, backend="pandas", add_filename=True
-    )
-
     output_dir = expand_outdir_and_mkdir(args.output_data_dir)
 
     metadata_field = args.input_metadata_field
-    print(f"Beginning metadata separation for {metadata_field}")
-    metadata_distribution = separate_by_metadata(
-        input_data,
+    print(f"Beginning metadata separation for {metadata_field}", flush=True)
+    for files in get_batched_files(
+        args.input_data_dir,
         output_dir,
-        metadata_field,
-        remove_metadata=args.remove_metadata_field,
-        output_type=args.output_file_type,
-    ).compute()
-    print(f"Finished metadata separation for {metadata_field}")
+        args.input_file_type,
+        batch_size=args.batch_size,
+    ):
+        input_data = read_data(
+            files,
+            file_type=args.input_file_type,
+            backend="pandas",
+            add_filename=True,
+        )
 
-    with open(args.output_metadata_distribution, "w") as fp:
-        json.dump(metadata_distribution, fp)
+        metadata_distribution = separate_by_metadata(
+            input_data,
+            output_dir,
+            metadata_field,
+            remove_metadata=args.remove_metadata_field,
+            output_type=args.output_file_type,
+        ).compute()
+
+        with open(args.output_metadata_distribution, "w") as fp:
+            json.dump(metadata_distribution, fp)
+
+    print(f"Finished metadata separation for {metadata_field}", flush=True)
 
     if args.remove_input_dir:
-        print(f"Removing all files in {args.input_data_dir}")
+        print(f"Removing all files in {args.input_data_dir}", flush=True)
         shutil.rmtree(args.input_data_dir)
-        print(f"Finished removing all files in {args.input_data_dir}")
+        print(f"Finished removing all files in {args.input_data_dir}", flush=True)
 
 
 def attach_args(
@@ -120,6 +129,12 @@ def attach_args(
         default="jsonl",
         help="File type the dataset will be written to. Supported file formats"
         " include 'jsonl' (default), 'pickle', or 'parquet'.",
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        help="Number of files to read into memory at a time.",
     )
 
     parser = add_distributed_args(parser)
