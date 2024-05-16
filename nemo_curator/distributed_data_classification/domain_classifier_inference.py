3423748b5daa0b544070bafe8e9df8ef7cd6d53b
Vibhu Jawa <vibhujawa@gmail.com>
2024-05-16 15:25:31 -0700

    domain classifier
    Signed-off-by: Vibhu Jawa <vibhujawa@gmail.com>

--- nemo_curator/distributed_data_classification/domain_classifier_inference.py
+++ nemo_curator/distributed_data_classification/domain_classifier_inference.py
@@ -17,130 +17,13 @@ import time
 import warnings
 
 os.environ["RAPIDS_NO_INITIALIZE"] = "1"
-from dataclasses import dataclass
-
-import torch
-import torch.nn as nn
-from crossfit import op
-from crossfit.backend.torch.hf.model import HFModel
-from packaging import version
-from transformers import AutoConfig, AutoModel
-from transformers import __version__ as TRANSFORMERS_VERSION
-from transformers.models.deberta_v2 import DebertaV2TokenizerFast
-
+from nemo_curator import DomainClassifier
+from nemo_curator.datasets import DocumentDataset
 from nemo_curator.distributed_data_classification.arg_utils import create_arg_parser
 from nemo_curator.utils.distributed_utils import get_client, read_data, write_to_disk
 from nemo_curator.utils.file_utils import get_remaining_files
 
 warnings.filterwarnings("ignore")
-
-
-@dataclass
-class Config:
-    model = "microsoft/deberta-v3-base"
-    fc_dropout = 0.2
-    max_len = 512
-
-
-class CustomModel(nn.Module):
-    def __init__(
-        self, config, out_dim, config_path=None, pretrained=False, autocast=False
-    ):
-        super().__init__()
-        self.config = config
-        if config_path is None:
-            self.config = AutoConfig.from_pretrained(
-                config.model, output_hidden_states=True
-            )
-        else:
-            self.config = torch.load(config_path)
-        if pretrained:
-            self.model = AutoModel.from_pretrained(config.model, config=self.config)
-        else:
-            self.model = AutoModel(self.config)
-        self.fc_dropout = nn.Dropout(config.fc_dropout)
-        self.fc = nn.Linear(self.config.hidden_size, out_dim)
-        self._init_weights(self.fc)
-        self.autocast = autocast
-
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def feature(self, input_ids, attention_mask):
-        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
-        last_hidden_states = outputs[0]
-        return last_hidden_states
-
-    def forward(self, batch):
-        if self.autocast:
-            with torch.autocast(device_type="cuda"):
-                feature = self.feature(batch["input_ids"], batch["attention_mask"])
-                output = self.fc(self.fc_dropout(feature))
-                output = output.to(torch.float32)
-        else:
-            feature = self.feature(batch["input_ids"], batch["attention_mask"])
-            output = self.fc(self.fc_dropout(feature))
-        return torch.softmax(output[:, 0, :], dim=1)
-
-
-def load_model(config, device, model_path, autocast):
-    """
-    This function loads the domain model and prepares it to be used for inference.
-    It is needed as an input to the `process_all_batches` function within the `inference_per_partition` function.
-
-    Args:
-        config: A config object.
-        device: A specified PyTorch device, such as torch.device("cuda") or torch.device("cpu").
-        model_path: The path to the model file.
-        autocast: Wether to autocast or not
-    Returns:
-        The loaded model.
-
-    """
-    model = CustomModel(
-        config, out_dim=27, config_path=None, pretrained=True, autocast=autocast
-    )
-    model = model.to(device)
-    if os.path.exists(model_path):
-        sd = torch.load(os.path.join(model_path), map_location="cpu")
-        sd = {k[7:] if k.startswith("module.") else k: sd[k] for k in sd.keys()}
-        if version.parse(TRANSFORMERS_VERSION) >= version.parse("4.31.0"):
-            sd.pop("model.embeddings.position_ids", None)
-        model.load_state_dict(sd, strict=True)
-    model.eval()
-    return model
-
-
-class DomainModel(HFModel):
-    def __init__(self, config, model_path=None, autocast=False):
-        self.config = config
-        self.model_path = model_path
-        self.autocast = autocast
-        super().__init__(self.config.model)
-
-    def load_model(self, device="cuda"):
-        return load_model(
-            self.config,
-            device=device,
-            model_path=self.model_path or self.path_or_name,
-            autocast=self.autocast,
-        )
-
-    def load_tokenizer(self):
-        return DebertaV2TokenizerFast.from_pretrained(self.config.model)
-
-    def load_config(self):
-        return AutoConfig.from_pretrained(self.path_or_name)
 
 
 def main():
@@ -177,7 +60,6 @@ def main():
     args = create_arg_parser().parse_args()
     print(f"Arguments parsed = {args}", flush=True)
     max_chars = 2000
-    batch_size = args.batch_size
 
     client = get_client(args, cluster_type="gpu")
     print("Starting domain classifier inference", flush=True)
@@ -193,6 +75,14 @@ def main():
     else:
         add_filename = True
 
+    domain_classifier = DomainClassifier(
+        model_file_name=args.model_file_name,
+        labels=labels,
+        max_chars=max_chars,
+        batch_size=args.batch_size,
+        autocast=args.autocast,
+    )
+
     for file_batch_id, i in enumerate(range(0, len(input_files), files_per_run)):
         batch_st = time.time()
         current_batch_files = input_files[i : i + files_per_run]
@@ -205,21 +95,8 @@ def main():
             file_type=args.input_file_type,
             add_filename=add_filename,
         )
-        df["sliced_text"] = df["text"].str.slice(0, max_chars)
-        columns_to_keep_list = df.columns.to_list()
-        columns_to_keep_list.remove("sliced_text")
-
-        model = DomainModel(
-            Config, model_path=args.model_file_name, autocast=args.autocast
-        )
-        pipe = op.Sequential(
-            op.Tokenizer(model, cols=["sliced_text"], tokenizer_type="sentencepiece"),
-            op.Predictor(model, sorted_data_loader=True, batch_size=batch_size),
-            op.Labeler(labels, cols=["preds"]),
-            repartition=df.npartitions,
-            keep_cols=columns_to_keep_list,
-        )
-        df = pipe(df)
+        df = domain_classifier(DocumentDataset(df)).df
+        print(f"Total input Dask DataFrame partitions {df.npartitions}", flush=True)
 
         write_to_disk(
             df=df,
@@ -242,3 +119,7 @@ def main():
 
 def console_script():
     main()
+
+
+if __name__ == "__main__":
+    console_script()
--- nemo_curator/distributed_data_classification/quality_classifier_inference.py
+++ nemo_curator/distributed_data_classification/quality_classifier_inference.py
@@ -17,196 +17,13 @@ import time
 import warnings
 
 os.environ["RAPIDS_NO_INITIALIZE"] = "1"
-import torch
-from transformers.models.deberta_v2 import DebertaV2TokenizerFast
-
+from nemo_curator import QualityClassifier
+from nemo_curator.datasets import DocumentDataset
 from nemo_curator.distributed_data_classification.arg_utils import create_arg_parser
-from nemo_curator.distributed_data_classification.pytorch_utils import (
-    CFG,
-    CustomModel,
-    TestDataset,
-    collate,
-)
-from nemo_curator.utils.distributed_utils import (
-    get_client,
-    load_object_on_worker,
-    process_all_batches,
-    read_data,
-    write_to_disk,
-)
+from nemo_curator.utils.distributed_utils import get_client, read_data, write_to_disk
 from nemo_curator.utils.file_utils import get_remaining_files
 
 warnings.filterwarnings("ignore")
-
-
-def inference_per_partition(
-    df,
-    max_chars,
-    batch_size,
-    num_workers,
-    model_file_name,
-    labels,
-    autocast,
-    include_model_name=False,
-):
-    """
-    This function runs quality classification on a subset of the data.
-    It loads the CFG, a data iterator, and then calls the `process_all_batches` function,
-    which loads the quality classifier and runs inference.
-    It also contains some additional logic to handle binary versus multiclass classification.
-
-    Args:
-        df: A Dask DataFrame partition with a "text" column and a dummy "quality_pred" column.
-        max_chars: The maximum number of characters allowed in the truncated text.
-        batch_size: How many samples per batch to load with PyTorch DataLoader.
-        num_workers: How many subprocesses to use for PyTorch DataLoader.
-        model_file_name: The path to the model file.
-        labels: The list of domain labels.
-        autocast: A boolean representing whether to perform inference with mixed precision.
-        include_model_name: A boolean representing whether to include the model name in the "quality_pred" column name.
-    Returns:
-        The input Dask DataFrame with the calculated "quality_pred" column.
-
-    """
-    cfg = cfg_per_partition()
-
-    dataset_valid = TestDataset(cfg, df, max_chars)
-    loader_valid = torch.utils.data.DataLoader(
-        dataset_valid,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-    )
-    device = torch.device("cuda")
-    if len(labels) == 1:
-        raise ValueError("Labels must be more than 1")
-
-    # binary case
-    if len(labels) == 2:
-        out_dim = 1
-        binary_classification = True
-    else:
-        out_dim = len(labels)
-        binary_classification = False
-
-    load_model_kwargs = {
-        "cfg": cfg,
-        "device": device,
-        "model_path": model_file_name,
-        "out_dim": out_dim,
-    }
-    run_inference_kwargs = {
-        "autocast": autocast,
-        "binary_classification": binary_classification,
-    }
-    st = time.time()
-    probs = process_all_batches(
-        loader_valid,
-        load_model,
-        load_model_kwargs,
-        run_inference,
-        run_inference_kwargs,
-    )
-    if binary_classification:
-        preds = (probs > 0.5).to(torch.int64).squeeze()
-    else:
-        preds = torch.argmax(probs, dim=1)
-    # TODO: Do this without a CPU roundtrip in the future
-    if include_model_name:
-        filename = os.path.basename(model_file_name)
-        df[f"quality_pred_{filename}"] = [
-            labels[i] for i in preds.to("cpu").numpy().tolist()
-        ]
-        df[f"quality_prob_{filename}"] = probs.to("cpu").numpy().tolist()
-    else:
-        df["quality_pred"] = [labels[i] for i in preds.to("cpu").numpy().tolist()]
-        df["quality_prob"] = probs.to("cpu").numpy().tolist()
-    et = time.time()
-    print(
-        f"Time taken for inference for num_batches: {len(loader_valid)} : {et-st} s",
-        flush=True,
-    )
-
-    return df
-
-
-def cfg_per_partition():
-    """
-    This function loads the CFG on the worker currently running the task.
-    See `load_object_on_worker` function.
-
-    Returns:
-        A CFG with a set `tokenizer` attribute.
-
-    """
-    return load_object_on_worker("cfg_with_tokenizer", load_cfg_with_tokenizer, {})
-
-
-def load_cfg_with_tokenizer():
-    """
-    This function loads the CFG needed for quality classification.
-
-    Returns:
-        A CFG with a set `tokenizer` attribute.
-
-    """
-    cfg = CFG(max_len=1024)
-    tokenizer = DebertaV2TokenizerFast.from_pretrained(cfg.model)
-    cfg.tokenizer = tokenizer
-    return cfg
-
-
-def load_model(cfg, device, model_path, out_dim):
-    """
-    This function loads the quality model and prepares it to be used for inference.
-    It is needed as an input to the `process_all_batches` function within the `inference_per_partition` function.
-
-    Args:
-        cfg: A CFG object.
-        device: A specified PyTorch device, such as torch.device("cuda") or torch.device("cpu").
-        model_path: The path to the model file.
-        out_dim: An integer which corresponds to the number of labels. Use 1 for binary classification.
-    Returns:
-        The loaded model.
-
-    """
-    model = CustomModel(cfg, out_dim=out_dim, config_path=None, pretrained=True)
-    model = model.to(device)
-    sd = torch.load(model_path, map_location="cpu")
-    if "model_state_dict" in sd:
-        sd = sd["model_state_dict"]
-    sd = {k[7:] if k.startswith("module.") else k: sd[k] for k in sd.keys()}
-    model.load_state_dict(sd, strict=True)
-    model.eval()
-    return model
-
-
-def run_inference(batch, model, autocast=False, binary_classification=False):
-    """
-    This function runs the quality classifier on a batch of data.
-    It is needed as an input to the `process_all_batches` function within the `inference_per_partition` function.
-
-    Args:
-        batch: A subset of the data as we are iterating through PyTorch DataLoader.
-        model: The loaded quality classification model.
-        autocast: A boolean representing whether to perform inference with mixed precision.
-        binary_classification: A boolean representing whether it is a binary classification model.
-    Returns:
-        A tensor of predictions.
-
-    """
-    with torch.no_grad():
-        batch = collate(batch)
-        if autocast:
-            with torch.autocast(device_type="cuda"):
-                out = model(batch)[:, 0, :]
-        else:
-            out = model(batch)[:, 0, :]
-        if binary_classification:
-            probs = torch.sigmoid(out)
-        else:
-            probs = torch.softmax(out, dim=1)
-    return probs
 
 
 def add_quality_model_specific_args(parser):
@@ -247,8 +64,6 @@ def main():
     labels = get_labels(args.num_labels)
     print(f"Arguments parsed = {args}", flush=True)
     max_chars = 6000
-    batch_size = args.batch_size
-    num_workers = 0
 
     client = get_client(args, cluster_type="gpu")
     print("Starting quality classifier inference", flush=True)
@@ -264,6 +79,14 @@ def main():
     else:
         add_filename = True
 
+    classifier = QualityClassifier(
+        model_file_name=args.model_file_name,
+        max_chars=max_chars,
+        labels=labels,
+        batch_size=args.batch_size,
+        autocast=args.autocast,
+    )
+
     for file_batch_id, i in enumerate(range(0, len(input_files), files_per_run)):
         batch_st = time.time()
         current_batch_files = input_files[i : i + files_per_run]
@@ -277,20 +100,7 @@ def main():
             add_filename=add_filename,
         )
         print(f"Total input Dask DataFrame partitions {df.npartitions}", flush=True)
-        meta_df = df._meta.copy()
-        meta_df["quality_pred"] = ["low"] * len(meta_df)
-        meta_df["quality_prob"] = [[0, 0, 1]] * len(meta_df)
-        df = df.map_partitions(
-            inference_per_partition,
-            max_chars,
-            batch_size,
-            num_workers,
-            args.model_file_name,
-            labels,
-            args.autocast,
-            meta=meta_df,
-            enforce_metadata=False,
-        )
+        df = classifier(DocumentDataset(df)).df
         write_to_disk(
             df=df,
             output_file_dir=args.output_file_path,
