d51716b5bdd42e9d2bb7627faa5e362d7cae9672
Ryan Wolf <rywolf@nvidia.com>
2024-05-24 10:29:06 -0700

    Add error for invalid snapshot order
    Signed-off-by: Ryan Wolf <rywolf@nvidia.com>

--- nemo_curator/utils/download_utils.py
+++ nemo_curator/utils/download_utils.py
@@ -36,6 +36,11 @@ def get_main_warc_paths(
     start_date = datetime.fromisocalendar(beg_year, beg_week, 1)
     end_date = datetime.fromisocalendar(end_year, end_week, 1)
 
+    if start_date > end_date:
+        raise ValueError(
+            f"Start snapshot '{start_snapshot}' is after end snapshot '{end_snapshot}'"
+        )
+
     if beg_year < 2013 or end_year < 2013:
         print("Warning: Only snapshots after 2013 are supported by this script")
 
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
+import pytest
+
+from nemo_curator.download.commoncrawl import get_common_crawl_urls
+
+
 class TestDownload:
     def test_imports(self):
         from nemo_curator.download import (
@@ -7,3 +14,26 @@ class TestDownload:
         )
 
         assert True
+
+    def test_common_crawl_urls(self):
+        start_snapshot = "2021-04"
+        end_snapshot = "2021-10"
+        urls = get_common_crawl_urls(start_snapshot, end_snapshot)
+
+        assert (
+            urls[0]
+            == "https://data.commoncrawl.org/crawl-data/CC-MAIN-2021-10/segments/1614178347293.1/warc/CC-MAIN-20210224165708-20210224195708-00000.warc.gz"
+        )
+        assert (
+            urls[-1]
+            == "https://data.commoncrawl.org/crawl-data/CC-MAIN-2021-04/segments/1610704847953.98/warc/CC-MAIN-20210128134124-20210128164124-00799.warc.gz"
+        )
+        assert len(urls) == 143840
+
+    def test_incorrect_snapshot_order(self):
+        with pytest.raises(ValueError):
+            end_snapshot = "2021-04"
+            start_snapshot = "2021-10"
+            urls = get_common_crawl_urls(start_snapshot, end_snapshot)
+
+            assert urls == []
