bd5bb559d4774ecb669cae209e1c2b28bee34dd1
Richard (Rick) Zamora <rzamora217@gmail.com>
2024-06-06 13:18:49 -0500

    make sure query-planning is disabled (#97)
    Signed-off-by: rjzamora <rzamora217@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,3 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import dask
+
+# Disable query planning before any tests are loaded
+# https://github.com/NVIDIA/NeMo-Curator/issues/73
+if dask.config.get("dataframe.query-planning") is True:
+    raise NotImplementedError(
+        "NeMo Curator does not support query planning yet. "
+        "Please disable query planning before importing "
+        "`nemo_curator`, `dask.dataframe` or `dask_cudf`."
+    )
+else:
+    dask.config.set({"dataframe.query-planning": False})
