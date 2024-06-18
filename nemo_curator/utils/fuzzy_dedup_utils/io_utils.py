93c8a2995b89111e01bae09065550b3c9271b292
miguelusque <miguelusque@users.noreply.github.com>
2024-06-18 05:19:15 -0400

    Fix missing default parameter
    Signed-off-by: miguelusque <miguelusque@users.noreply.github.com>

--- nemo_curator/utils/fuzzy_dedup_utils/io_utils.py
+++ nemo_curator/utils/fuzzy_dedup_utils/io_utils.py
@@ -60,8 +60,9 @@ def _read_json_func(
 
 
 def get_text_ddf_from_json_path_with_blocksize(
-    input_data_paths, num_files, blocksize, id_column, text_column, input_meta
+    input_data_paths, num_files, blocksize, id_column, text_column, input_meta=None
 ):
+    print("Hello world!")
     data_paths = [
         entry.path for data_path in input_data_paths for entry in os.scandir(data_path)
     ]
