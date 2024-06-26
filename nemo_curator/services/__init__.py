f3e6309235b11ca0798d4283872334e8b856c70a
Ryan Wolf <rywolf@nvidia.com>
2024-06-26 16:14:51 -0700

    Make imports easier
    Signed-off-by: Ryan Wolf <rywolf@nvidia.com>

--- nemo_curator/services/__init__.py
+++ nemo_curator/services/__init__.py
@@ -1,2 +1,4 @@
 from .model_client import AsyncLLMClient, LLMClient
 from .openai_client import AsyncOpenAIClient, OpenAIClient
+
+__all__ = ["AsyncLLMClient", "LLMClient", "AsyncOpenAIClient", "OpenAIClient"]
---
+++ nemo_curator/synthetic/__init__.py
@@ -0,0 +1,4 @@
+from .prompts import DEFAULT_META_TOPICS_PROMPT_TEMPLATE
+from .ultrachat import UltraChatGenerator
+
+__all__ = ["UltraChatGenerator", "DEFAULT_META_TOPICS_PROMPT_TEMPLATE"]
