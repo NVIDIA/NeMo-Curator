79beb617acd9cc2a915a5564468f12d4767187f9
Vibhu Jawa <vibhujawa@gmail.com>
2024-06-27 03:05:25 -0700

    cleanup semdedup_crossfit
    Signed-off-by: Vibhu Jawa <vibhujawa@gmail.com>

--- nemo_curator/modules/semdedup/extract_dedup_data.py
+++ nemo_curator/modules/semdedup/extract_dedup_data.py
@@ -1,19 +1,32 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
+import logging
 import os
 import pickle
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
 import yaml
 from tqdm import tqdm
+from utils import get_logger
 
 
 def extract_pruned_data(
+    id_col,
+    id_type,
     sorted_clusters_path,
     semdedup_pruning_tables_path,
     eps,
@@ -24,34 +37,42 @@ def extract_pruned_data(
     ## -- duplicates we want to keep/remove.
     dedup_clusters = []
     total = 0
+
     for cluster_id in tqdm(range(num_clusters)):
 
-        cluster_i = np.load(
-            os.path.join(sorted_clusters_path, f"cluster_{cluster_id}.npy")
-        )
+        sorted_fname = os.path.join(sorted_clusters_path, f"cluster_{cluster_id}.npy")
+        if not os.path.exists(sorted_fname):
+            logger.info(f"{sorted_fname} not exist. Continue.")
+            continue
+        cluster_i = np.load(sorted_fname)
 
         df_cluster_i = pd.DataFrame(
             {
-                "adlr_id": cluster_i[:, 0],
-                "global_id": cluster_i[:, 1],
-                "dist": cluster_i[:, 2],
-                "cluster": cluster_i[:, 3],
+                id_col: cluster_i[:, 0],
+                "dist": cluster_i[:, 1],
+                "cluster": cluster_i[:, 2],
             }
         )
-        df_cluster_i.adlr_id = df_cluster_i.adlr_id.astype("str")
-        df_cluster_i.global_id = df_cluster_i.global_id.astype("int32")
+        df_cluster_i[id_col] = df_cluster_i[id_col].astype(id_type)
         df_cluster_i.dist = df_cluster_i.dist.astype("float32")
         df_cluster_i.cluster = df_cluster_i.cluster.astype("int32")
         total += df_cluster_i.shape[0]
 
-        print("==>", semdedup_pruning_tables_path, cluster_id)
+        logger.info(
+            f"semdedup_pruning_tables_path: {semdedup_pruning_tables_path}, cluster_id: {cluster_id}"
+        )
+
         with open(
             f"{semdedup_pruning_tables_path}/cluster_{cluster_id}.pkl", "rb"
         ) as file:
             semdedup_pruning_tables = pickle.load(file)
 
         if semdedup_pruning_tables.shape[0] == 1:
-            print("[]", cluster_id, semdedup_pruning_tables.shape, df_cluster_i.shape)
+            logger.info(
+                f"""cluster_id: {cluster_id},
+                    semdedup_pruning_tables.shape: {semdedup_pruning_tables.shape},
+                    df_cluster_i.shape: {df_cluster_i.shape}"""
+            )
             continue
 
         ## -- See which examples to keep in this cluster.
@@ -59,21 +80,21 @@ def extract_pruned_data(
 
         items_to_keep = semdedup_pruning_tables[
             semdedup_pruning_tables[f"eps={eps}"] == False
-        ]["global_id"].tolist()
+        ]["id"].tolist()
 
         if "indices" in semdedup_pruning_tables.columns:
             cluster_i = cluster_i[semdedup_pruning_tables["indices"]]
 
         ## -- retrieve only the examples we want and add to the list.
-        dedup_cluster = df_cluster_i[df_cluster_i.global_id.isin(items_to_keep)]
+        dedup_cluster = df_cluster_i[df_cluster_i[id_col].isin(items_to_keep)]
         dedup_clusters.append(dedup_cluster)
 
     result = pd.concat(dedup_clusters)
     result.to_csv(output_csv_path, index=False)
     num_removed = total - result.shape[0]
 
-    print(f"DONE saving {result.shape[0]} out of {total}. Removed: {num_removed}")
-    return
+    logger.info(f"DONE saving {result.shape[0]} out of {total}. Removed: {num_removed}")
+    return result.shape[0], num_removed, total
 
 
 if __name__ == "__main__":
@@ -83,16 +104,58 @@ if __name__ == "__main__":
 
     root = params["root"]
     save_loc = params["clustering"]["save_loc"]
-    eps = params["extract_dedup"]["eps"]
 
-    output_csv_path = f"{root}/{save_loc}/results_eps_{eps}.csv"
-    sorted_clusters_path = f"{root}/{save_loc}/sorted"
-    semdedup_pruning_tables_path = f"{root}/{save_loc}/dataframes"
-    os.makedirs(semdedup_pruning_tables_path, exist_ok=True)
-    extract_pruned_data(
-        sorted_clusters_path=sorted_clusters_path,
-        semdedup_pruning_tables_path=semdedup_pruning_tables_path,
-        eps=eps,
-        num_clusters=params["clustering"]["num_clusters"],
-        output_csv_path=output_csv_path,
+    logger = get_logger(
+        file_name=f"{root}/{save_loc}/extract_dedup_data.log",
+        level=logging.INFO,
+        stdout=True,
     )
+
+    if params["extract_dedup"]["use_eps_from_yml"]:
+        eps = params["extract_dedup"]["eps"]
+        eps_list = [float(x) for x in eps.split(" ")]
+    else:
+        eps_list1 = [1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6]
+        eps_list2 = [0.1 + x * 0.005 for x in range(34)]
+        eps_list = eps_list1 + eps_list2
+    kept_list = []
+    removed_list = []
+    total_list = []
+    id_col = params["id_col"]["name"]
+    id_type = params["id_col"]["type"]
+
+    dt1 = datetime.now()
+    logger.info(f"Start: {dt1}")
+
+    for eps in eps_list:
+        output_csv_path = f"{root}/{save_loc}/results_eps_{eps}.csv"
+        sorted_clusters_path = f"{root}/{save_loc}/sorted"
+        semdedup_pruning_tables_path = f"{root}/{save_loc}/dataframes"
+        os.makedirs(semdedup_pruning_tables_path, exist_ok=True)
+        kept, removed, total = extract_pruned_data(
+            id_col=id_col,
+            id_type=id_type,
+            sorted_clusters_path=sorted_clusters_path,
+            semdedup_pruning_tables_path=semdedup_pruning_tables_path,
+            eps=eps,
+            num_clusters=params["clustering"]["num_clusters"],
+            output_csv_path=output_csv_path,
+        )
+        kept_list.append(kept)
+        removed_list.append(removed)
+        total_list.append(total)
+
+    dict = {
+        "eps": eps_list,
+        "kept": kept_list,
+        "removed": removed_list,
+        "total": total_list,
+    }
+    df = pd.DataFrame(dict)
+    summary_file = f"{root}/{save_loc}/summary.csv"
+    df.to_csv(summary_file, index=False)
+
+    dt2 = datetime.now()
+    logger.info(f"End: {dt2}")
+    elapse = (dt2 - dt1).total_seconds() / 60
+    logger.info(f"elapse: {elapse}")
--- nemo_curator/modules/semdedup/semdedup_crossfit.py
+++ nemo_curator/modules/semdedup/semdedup_crossfit.py
@@ -1,15 +1,20 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-import argparse
 import logging
-import math
 import os
 import pathlib
-import pdb
 import pickle
 import random
 from datetime import datetime
@@ -17,16 +22,16 @@ from glob import glob
 from typing import List, Tuple
 
 import cudf
-import cupy as cp
-import dask_cudf
 import numpy as np
 import pandas as pd
 import torch
 import yaml
-from dask.distributed import Client, LocalCluster
-from dask_cuda import LocalCUDACluster
 from tqdm import tqdm
 from utils import get_logger
+
+from nemo_curator.modules.semdedup.utils import parse_arguments
+from nemo_curator.utils.distributed_utils import get_client
+from nemo_curator.utils.script_utils import parse_client_args
 
 
 def _semdedup(cluster_reps, device):
@@ -48,34 +53,44 @@ def _semdedup(cluster_reps, device):
     return M, M1
 
 
-def get_cluster_reps(cluster_id, emb_by_clust_loc, id_col):
-    cluster_i_path = os.path.join(emb_by_clust_loc, f"nearest_cent={cluster_id:0.1f}")
+def get_cluster_reps(cluster_id, emb_by_clust_loc, id_col, sorted_ids):
+    cluster_i_path = os.path.join(emb_by_clust_loc, f"nearest_cent={cluster_id}")
     cluster_reps = cudf.read_parquet(
         cluster_i_path, columns=["embeddings", id_col]
-    ).sort_values(by=[id_col])
-    cluster_reps = torch.tensor(
-        cluster_reps["embeddings"].list.leaves.values.reshape(len(cluster_reps), -1),
-        device="cuda",
-    )
+    ).sort_values(by=id_col)
+    num = cluster_reps.shape[0]
+
+    df_ = pd.DataFrame(
+        {"sorted_ids": sorted_ids, "inverse_sort": list(range(num))}
+    ).sort_values(by="sorted_ids")
+    cluster_reps["inverse_sort_id"] = df_["inverse_sort"].values
+    cluster_reps = cluster_reps.sort_values(by="inverse_sort_id")
+
+    cluster_reps = torch.Tensor(
+        cluster_reps["embeddings"].list.leaves.values.reshape(len(cluster_reps), -1)
+    )  # , device="cuda")
     return cluster_reps
 
 
-def semdedup(client, params):
+def semdedup(args):
     dt1 = datetime.now()
     logger.info(f"semdedup: start {dt1}")
 
-    end_cluster = params["clustering"]["num_clusters"]
-    root = params["root"]
-    emb_pqt_loc = f'{root}/{params["embeddings"]["emb_parquet_path"]}'
+    end_cluster = args.clustering["num_clusters"]
+    root = args.root
+    emb_pqt_loc = os.path.join(root, args.embeddings["output_data_dir"])
+
+    eps_list1 = [1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6]
+    eps_list2 = [0.1 + x * 0.005 for x in range(34)]
+    eps_list = eps_list1 + eps_list2
     logger.info(f"emb_pqt_loc: {emb_pqt_loc}")
 
-    emb_size = params["embeddings"]["emb_size"]
-    id_col = params["id_col"]["name"]
-    id_col_type = params["id_col"]["type"]
-    save_loc = f'{root}/{params["clustering"]["save_loc"]}'
-    result_dir = f"{save_loc}/dataframes"
+    id_col = args.id_col["name"]
+    id_col_type = args.id_col["type"]
+    save_loc = f'{root}/{args.clustering["save_loc"]}'
     emb_by_clust_loc = pathlib.Path(save_loc, "embs_by_nearest_center")
 
+    # TODO: Use dask-cuda for this
     for cluster_id in tqdm(range(end_cluster)):
         df_file_loc = os.path.join(save_loc, f"dataframes/cluster_{cluster_id}.pkl")
 
@@ -85,9 +100,13 @@ def semdedup(client, params):
 
         ## -- load cluster i representations
         sorted_clusters_path = f"{save_loc}/sorted"
-        cluster_i = np.load(
-            os.path.join(sorted_clusters_path, f"cluster_{cluster_id}.npy")
-        )
+        sorted_file = os.path.join(sorted_clusters_path, f"cluster_{cluster_id}.npy")
+        if not os.path.exists(sorted_file):
+            logger.info(f"{sorted_file} does not exist. Continue")
+            continue
+
+        cluster_i = np.load(sorted_file)
+
         # 1) store cluster size
         cluster_size = cluster_i.shape[0]
         logger.info(f"{cluster_id}: cluster_size: {cluster_size}")
@@ -95,7 +114,7 @@ def semdedup(client, params):
         if cluster_size == 1:
             points_to_remove_df = pd.DataFrame()
             points_to_remove_df["indices"] = [0]
-            for eps in params["eps_list"]:
+            for eps in eps_list:
                 ## We need to remove a point from the dataset when its pairwise similarity to other point is > 1-ebs
                 points_to_remove_df[f"eps={eps}"] = [False]
             if save_loc != "":
@@ -108,41 +127,32 @@ def semdedup(client, params):
         clutser_items_indices = list(range(cluster_size))
 
         ## -- OR: shuffle cluster to keep random example from each group
-        which_to_keep = params["semdedup"]["which_to_keep"]
-        if which_to_keep.lower() == "random":
+        which_to_keep = args.semdedup["which_to_keep"].lower()
+        if which_to_keep == "random":
             random.shuffle(clutser_items_indices)
             cluster_i = cluster_i[clutser_items_indices]
         ## -- OR: reverse cluster to keep easy examples
-        if which_to_keep == "easy":
+        elif which_to_keep == "easy":
             clutser_items_indices = clutser_items_indices[::-1]
             cluster_i = cluster_i[clutser_items_indices]
 
-        ## -- indices for cluster items in the dataset
-        cluster_global_ids = cluster_i[:, 1].astype("int32")
-        cluster_global_ids.sort()
-
         # can be adlr_id (nemo-curator data) or id (c4 data)
-        cluster_ids = cluster_i[:, 0].astype(id_col_type)
-        cluster_ids.sort()
+        text_ids = cluster_i[:, 0].astype(id_col_type)
 
-        cluster_reps = get_cluster_reps(cluster_id, emb_by_clust_loc, id_col)
-        M, M1 = _semdedup(cluster_reps, params["use_gpu"])
-        assert cluster_reps.shape[0] == len(cluster_ids)
+        cluster_reps = get_cluster_reps(cluster_id, emb_by_clust_loc, id_col, text_ids)
+        M, M1 = _semdedup(cluster_reps, "cuda")
+        assert cluster_reps.shape[0] == len(text_ids)
 
         idx = [i for i in range(len(M1))]
-        M1_id = [cluster_ids[m] for m in M1]
-        M1_global = [cluster_global_ids[m] for m in M1]
+        M1_id = [text_ids[m] for m in M1]
 
         points_to_remove_df = pd.DataFrame()
         points_to_remove_df["indices"] = clutser_items_indices
-        points_to_remove_df["global_id"] = cluster_global_ids
-        points_to_remove_df["id"] = cluster_ids
-        points_to_remove_df["max_id"] = M1
-        points_to_remove_df["max_global_id"] = M1_global
+        points_to_remove_df["id"] = text_ids
         points_to_remove_df["max_id"] = M1_id
         points_to_remove_df["cosine_sim_score"] = M.numpy().tolist()
 
-        for eps in params["eps_list"]:
+        for eps in eps_list:
             ## -- 5) We need to remove a point from the dataset when its pairwise similarity to other point is > 1-ebs
             eps_points_to_remove = M > 1 - eps
             points_to_remove_df[f"eps={eps}"] = eps_points_to_remove
@@ -160,32 +170,25 @@ def semdedup(client, params):
 
 
 if __name__ == "__main__":
-    config_file = "./configs_cf.yml"
-    with open(config_file, "r") as y_file:
-        params = yaml.load(y_file, Loader=yaml.FullLoader)
 
-    save_loc = f"{params['root']}/{params['clustering']['save_loc']}"
+    args = parse_arguments()
+    # client = get_client(**parse_client_args(args))
+
+    save_loc = os.path.join(args.root, args.clustering["save_loc"])
+
     os.makedirs(save_loc, exist_ok=True)
-    os.makedirs(f"{save_loc}/dataframes", exist_ok=True)
+    os.makedirs(os.path.join(save_loc, "dataframes"), exist_ok=True)
 
     logger = get_logger(
-        file_name=f"{save_loc}/semdedup-logs.log",
+        file_name=f"{save_loc}/semdedup.log",
         level=logging.INFO,
         stdout=True,
     )
-
-    params["eps_list"] = [1.0e-2, 1.0e-3, 1.0e-4, 1.0e-5, 1.0e-6]
-    use_gpu = torch.cuda.is_available()
-
-    params["use_gpu"] = "cuda" if use_gpu else "cpu"
-
-    logger.info(params)
+    logger.info(args)
     dt1 = datetime.now()
     logger.info(f"Start: {dt1}")
 
-    cluster = LocalCUDACluster() if use_gpu else LocalCluster()
-    client = Client(cluster)
-    semdedup(client, params)
+    semdedup(args)
 
     dt2 = datetime.now()
     logger.info(f"End: {dt2}")
