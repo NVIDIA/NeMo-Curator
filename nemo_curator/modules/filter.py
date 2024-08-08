52ba08ed0c6fe36a354a5d030050275ed900320f
Shuoyang Ding <shuoyangd@nvidia.com>
2024-08-08 14:42:23 -0700

    fix pre-commit complaint
    Signed-off-by: Shuoyang Ding <shuoyangd@nvidia.com>

--- nemo_curator/modules/filter.py
+++ nemo_curator/modules/filter.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, Optional, List, Union
+from typing import Callable, List, Optional, Union
 
 import pandas as pd
 from dask.array import logical_and
