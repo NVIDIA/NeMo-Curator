20845496d46f1a97513aa5d81a0c343b6b302b81
miguelusque <miguelusque@users.noreply.github.com>
2024-09-22 06:02:54 -0400

    Display only Dask error messages or above
    Signed-off-by: miguelusque <miguelusque@users.noreply.github.com>

--- nemo_curator/scripts/separate_by_metadata.py
+++ nemo_curator/scripts/separate_by_metadata.py
@@ -14,7 +14,10 @@
 
 import argparse
 import json
+import logging
 import shutil
+
+from dask.distributed.utils import silence_logging_cmgr
 
 from nemo_curator.utils.distributed_utils import get_client
 from nemo_curator.utils.file_utils import separate_by_metadata
@@ -24,36 +27,40 @@ from nemo_curator.utils.script_utils import ArgumentHelper
 def main(args):
     print(f"Beginning metadata separation for {args.input_metadata_field}")
 
-    # Initializes or connects to a Dask cluster.
-    client = get_client(**ArgumentHelper.parse_client_args(args))
+    with silence_logging_cmgr(logging.ERROR):
+        # Initializes a Dask cluster.
+        client = get_client(**ArgumentHelper.parse_client_args(args))
 
-    # Separete corpus by metadata
-    metadata_distribution = separate_by_metadata(
-        args.input_data_dir,
-        args.output_data_dir,
-        args.input_metadata_field,
-        remove_metadata=args.remove_metadata_field,
-        output_type=args.output_file_type,
-        value_selection_filter=args.value_selection_filter,
-        value_exclusion_filter=args.value_exclusion_filter,
-    ).compute()
+        # Separete corpus by metadata
+        metadata_distribution = separate_by_metadata(
+            args.input_data_dir,
+            args.output_data_dir,
+            args.input_metadata_field,
+            remove_metadata=args.remove_metadata_field,
+            output_type=args.output_file_type,
+            value_selection_filter=args.value_selection_filter,
+            value_exclusion_filter=args.value_exclusion_filter,
+        ).compute()
 
-    with open(args.output_metadata_distribution, "w") as fp:
-        json.dump(metadata_distribution, fp)
+        with open(args.output_metadata_distribution, "w") as fp:
+            json.dump(metadata_distribution, fp)
 
-    if args.remove_input_dir:
-        print(f"Removing all files in {args.input_data_dir}")
-        shutil.rmtree(args.input_data_dir)
-        print(f"Finished removing all files in {args.input_data_dir}")
+        if args.remove_input_dir:
+            print(f"Removing all files in {args.input_data_dir}")
+            shutil.rmtree(args.input_data_dir)
+            print(f"Finished removing all files in {args.input_data_dir}")
 
-    print(
-        f"Finished separating {args.input_metadata_field} metadata\n"
-        "Dask messages from here may relate to cluster shutdown.",
-        flush=True,
-    )
+        print(
+            f"Finished separating {args.input_metadata_field} metadata\n"
+            "Dask messages from here may relate to cluster shutdown.",
+            flush=True,
+        )
 
-    # Shut down the cluster
-    client.shutdown()
+        # Shut down the cluster
+        client.shutdown()
+
+        # Close the client
+        client.close()
 
 
 def attach_args(
