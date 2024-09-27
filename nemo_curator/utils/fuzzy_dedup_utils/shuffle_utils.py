48f8e33d623bd61d46d3433e5a7a0888377d535d
Richard (Rick) Zamora <rzamora217@gmail.com>
2024-09-27 17:15:05 -0500

    Add dask query-planning support (#139)
    * start adding dask-expr support
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    * add query_planning_enabled util
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    * add global keyword
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    * Forgot to remove top level query-planning check
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    * fix other shuffle-arg problems that don't 'work' with dask-expr
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    * remove name arg usage for now
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    * fix bugs
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    
    ---------
    
    Signed-off-by: rjzamora <rzamora217@gmail.com>
    Signed-off-by: Vinay Raman <viraman@nvidia.com>

--- nemo_curator/utils/fuzzy_dedup_utils/shuffle_utils.py
+++ nemo_curator/utils/fuzzy_dedup_utils/shuffle_utils.py
@@ -16,10 +16,9 @@ import cudf
 import dask_cuda
 import numpy as np
 from dask import config
-from dask.dataframe.shuffle import rearrange_by_column
-from dask_cuda.explicit_comms.dataframe.shuffle import shuffle as explicit_comms_shuffle
 from packaging.version import Version
 
+from nemo_curator._compat import query_planning_enabled
 from nemo_curator.utils.fuzzy_dedup_utils.output_map_utils import (
     build_partition,
     get_agg_text_bytes_df,
@@ -53,6 +52,10 @@ def rearange_by_column_direct(
 ):
     # Execute a "direct" shuffle operation without staging
     if config.get("explicit-comms", excomms_default):
+        from dask_cuda.explicit_comms.dataframe.shuffle import (
+            shuffle as explicit_comms_shuffle,
+        )
+
         # Use explicit comms unless the user has
         # disabled it with the dask config system,
         # or we are using an older version of dask-cuda
@@ -62,7 +65,28 @@ def rearange_by_column_direct(
             npartitions=npartitions,
             ignore_index=ignore_index,
         )
+
+    elif query_planning_enabled():
+        from dask_expr._collection import new_collection
+        from dask_expr._shuffle import RearrangeByColumn
+
+        # Use the internal dask-expr API
+        return new_collection(
+            RearrangeByColumn(
+                frame=df.expr,
+                partitioning_index=col,
+                npartitions_out=npartitions,
+                ignore_index=ignore_index,
+                method="tasks",
+                # Prevent staged shuffling by setting max_branch
+                # to the number of input partitions + 1
+                options={"max_branch": npartitions + 1},
+            )
+        )
+
     else:
+        from dask.dataframe.shuffle import rearrange_by_column
+
         return rearrange_by_column(
             df,
             col=col,
