ea3df6c6f6b36ef0c9f4f6043d9068a59112762d
viraman <viraman@nvidia.com>
2024-10-16 18:09:02 -0700

    pre-commit + signoff
    Signed-off-by: viraman <viraman@nvidia.com>
    Signed-off-by: Vinay Raman <viraman@nvidia.com>

--- tutorials/synthetic-retrieval-evaluation-customization/filters.py
+++ tutorials/synthetic-retrieval-evaluation-customization/filters.py
@@ -12,13 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import itertools
+from typing import List, Union
+
 import dask
+import dask.dataframe as dd
 import fasttext
 import numpy as np
 import pandas as pd
-import dask.dataframe as dd
-from typing import Union, List
-import itertools
 from omegaconf import DictConfig
 from openai import OpenAI
 
@@ -29,13 +30,11 @@ from nemo_curator.utils.distributed_utils import NoWorkerError, load_object_on_w
 
 class EasinessFilter(DocumentFilter):
 
-    def __init__(self, 
-                 cfg: DictConfig,
-                 text_fields: List[str] = ["text", "question"]):
-        
+    def __init__(self, cfg: DictConfig, text_fields: List[str] = ["text", "question"]):
+
         self._name = "easiness_filter"
         if "easiness_filter" in cfg:
-            self.filter_cfg = cfg['easiness_filter']['filter_cfg']
+            self.filter_cfg = cfg["easiness_filter"]["filter_cfg"]
         else:
             raise Exception("Error: Config doesn't have easiness filter")
         self.base_url = self.filter_cfg.base_url
@@ -46,28 +45,23 @@ class EasinessFilter(DocumentFilter):
             self.truncate = self.filter_cfg.truncate
         else:
             self.truncate = "NONE"
-        self.client = OpenAI(
-            base_url=self.base_url,
-            api_key=self.api_key
-            )
+        self.client = OpenAI(base_url=self.base_url, api_key=self.api_key)
         self.batch_size = self.filter_cfg.batch_size
         self.text_fields = text_fields
-        
 
     @batched
     def score_document(self, df: Union[pd.DataFrame, dd.DataFrame]):
-        
-        document_score = self._calc_similarity_nim(df[self.text_fields[0]].to_list(), 
-                                                   df[self.text_fields[1]].to_list())
-        return pd.Series(document_score)
 
+        document_score = self._calc_similarity_nim(
+            df[self.text_fields[0]].to_list(), df[self.text_fields[1]].to_list()
+        )
+        return pd.Series(document_score)
 
     @batched
     def keep_document(self, scores: pd.Series):
         filter_threshold = np.percentile(scores, self.percentile)
-        
+
         return scores <= filter_threshold
-        
 
     def _get_nim_embedding(self, text, input_type):
         # Obtain embeddings from nim model
@@ -75,44 +69,40 @@ class EasinessFilter(DocumentFilter):
             input_ = text
         elif isinstance(text, str):
             input_ = [text]
-            
+
         try:
             response = self.client.embeddings.create(
-                    input= input_,
-                    model= self.nim_model,
-                    encoding_format="float",
-                    extra_body={"input_type": input_type, 
-                                "truncate": self.truncate}
+                input=input_,
+                model=self.nim_model,
+                encoding_format="float",
+                extra_body={"input_type": input_type, "truncate": self.truncate},
             )
         except Exception as e:
-            print (f'Error: {e}')
+            print(f"Error: {e}")
             response = None
-            
+
         if response:
-            if isinstance(text, list): 
+            if isinstance(text, list):
                 embeddings = [r.embedding for r in response.data]
             elif isinstance(text, str):
                 embeddings = response.data[0].embedding
             return embeddings
         else:
-            return [] 
-
+            return []
 
     def _calc_similarity_nim(self, context, question):
-        #cosine similarity 
-        doc_embed = self._get_nim_embedding(text=context, 
-                                          input_type='passage')
-        q_embed = self._get_nim_embedding(text=question,
-                                        input_type='query')
+        # cosine similarity
+        doc_embed = self._get_nim_embedding(text=context, input_type="passage")
+        q_embed = self._get_nim_embedding(text=question, input_type="query")
         if isinstance(context, list) and isinstance(question, list):
             if doc_embed and q_embed:
                 sim = np.diag(np.dot(np.array(doc_embed), np.array(q_embed).T))
             else:
-                sim = np.zeros(len(context)) 
-        else: 
+                sim = np.zeros(len(context))
+        else:
             if doc_embed and q_embed:
                 sim = np.dot(doc_embed, q_embed)
             else:
-                sim = 0.
+                sim = 0.0
 
         return sim
--- tutorials/synthetic-retrieval-evaluation-customization/retriever_evalset_generator.py
+++ tutorials/synthetic-retrieval-evaluation-customization/retriever_evalset_generator.py
@@ -13,23 +13,23 @@
 # limitations under the License.
 
 import importlib
+import os
+import re
 from abc import ABC, abstractmethod
 from typing import Any
+
+from omegaconf import DictConfig, OmegaConf
+from openai import OpenAI
+from tqdm import tqdm
+
+from nemo_curator import OpenAIClient
 from nemo_curator.datasets import DocumentDataset
-from nemo_curator.utils.module_utils import is_batched
-from nemo_curator.utils.decorators import batched
 from nemo_curator.filters.doc_filter import DocumentFilter
-from omegaconf import DictConfig
+from nemo_curator.synthetic import NemotronGenerator
 from nemo_curator.synthetic.generator import SyntheticDataGenerator
+from nemo_curator.utils.decorators import batched
 from nemo_curator.utils.module_utils import is_batched
 
-from openai import OpenAI
-from nemo_curator import OpenAIClient
-from nemo_curator.synthetic import NemotronGenerator
-from omegaconf import OmegaConf
-from tqdm import tqdm
-import re
-import os
 
 class RetrieverEvalSetGenerator(SyntheticDataGenerator):
 
@@ -38,49 +38,56 @@ class RetrieverEvalSetGenerator(SyntheticDataGenerator):
         self._name = self.__class__.__name__
         self.cfg = pipeline_config
 
-        #synchronous
+        # synchronous
         self.openai_client = OpenAI(
             base_url="https://integrate.api.nvidia.com/v1",
-            api_key= os.environ['NVIDIA_API_KEY'] #TODO api_key
+            api_key=os.environ["NVIDIA_API_KEY"],  # TODO api_key
         )
         self.client = OpenAIClient(self.openai_client)
         self.generator = NemotronGenerator(self.client)
 
-        #TODO asynchronous
+        # TODO asynchronous
 
     def load_pipeline_config(self, cfg_path: str):
         self.cfg = OmegaConf.load(cfg_path)
 
     def validate_config(self):
-        return True #TODO complete this
+        return True  # TODO complete this
 
-    
     def init_pipeline_params(self):
 
         if self.validate_config:
-            self.sys_prompt = self.cfg['qa_generator']['generate_config']['system_prompt']
-            self.user_prompt_template = self.cfg['qa_generator']['generate_config']['user_prompt_template']
-            self.generator_model = self.cfg['qa_generator']['model']
-            self.generator_model_kwargs = self.cfg['qa_generator']['model_config']
-            self.num_qs = self.cfg['qa_generator']['generate_config']['num_questions']
-            self.easiness_filter_cfg = self.cfg['easiness_filter']['filter_cfg']
+            self.sys_prompt = self.cfg["qa_generator"]["generate_config"][
+                "system_prompt"
+            ]
+            self.user_prompt_template = self.cfg["qa_generator"]["generate_config"][
+                "user_prompt_template"
+            ]
+            self.generator_model = self.cfg["qa_generator"]["model"]
+            self.generator_model_kwargs = self.cfg["qa_generator"]["model_config"]
+            self.num_qs = self.cfg["qa_generator"]["generate_config"]["num_questions"]
+            self.easiness_filter_cfg = self.cfg["easiness_filter"]["filter_cfg"]
         else:
             raise Exception("Validation Error: incorrect pipeline config file")
 
-    
     def run(self, dataset: DocumentDataset) -> DocumentDataset:
 
         df = dataset.df
-        df['llm_response'] = df['text'].apply(self._generate, 
-                                              meta=('llm_response', 'str'))
-        df['qa_pairs'] = df['llm_response'].apply(self._parse_response, 
-                                                   meta=('qa_pairs', 'object'))
-        df = df.explode('qa_pairs').reset_index(drop=True)
-        df['question'] = df['qa_pairs'].apply(lambda x: x['question'], meta=('question','str'))
-        df['answer'] = df['qa_pairs'].apply(lambda x: x['answer'], meta=('answer', 'str'))
-        return DocumentDataset(df[['_id','text','title','question','answer']])
+        df["llm_response"] = df["text"].apply(
+            self._generate, meta=("llm_response", "str")
+        )
+        df["qa_pairs"] = df["llm_response"].apply(
+            self._parse_response, meta=("qa_pairs", "object")
+        )
+        df = df.explode("qa_pairs").reset_index(drop=True)
+        df["question"] = df["qa_pairs"].apply(
+            lambda x: x["question"], meta=("question", "str")
+        )
+        df["answer"] = df["qa_pairs"].apply(
+            lambda x: x["answer"], meta=("answer", "str")
+        )
+        return DocumentDataset(df[["_id", "text", "title", "question", "answer"]])
 
-        
     def _parse_response(self, llm_response: str) -> Any:
         qa_pairs = []
         qa_list = llm_response.split("Question")[1:]
@@ -93,18 +100,16 @@ class RetrieverEvalSetGenerator(SyntheticDataGenerator):
                     explanation = qas[1].split("Explanation")[1].strip()  # Not used
                 else:
                     a = qas[1].split(":")[1].strip()
-                qa_pairs.append({"question": q,
-                                 "answer": a})
+                qa_pairs.append({"question": q, "answer": a})
         except Exception as e:
             print(f"error: {e}")
         return qa_pairs
 
-    
     def _generate(self, doc_text):
         return self.generator.generate_closed_qa_instructions(
             document=doc_text,
-            prompt_template = self.sys_prompt+ '\n' + self.user_prompt_template,
-            n_openlines= self.num_qs,
+            prompt_template=self.sys_prompt + "\n" + self.user_prompt_template,
+            n_openlines=self.num_qs,
             model=self.generator_model,
-            model_kwargs = self.generator_model_kwargs
+            model_kwargs=self.generator_model_kwargs,
         )[0]
