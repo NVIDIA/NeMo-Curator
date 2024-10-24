994e3bb5b3d10750ae1912967d3c23598481d9ff
Sarah Yurick <sarahyurick@gmail.com>
2024-10-24 12:47:48 -0700

    add classifiers and semdedup
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- nemo_curator/scripts/classifiers/fineweb_edu_classifier_inference.py
+++ nemo_curator/scripts/classifiers/fineweb_edu_classifier_inference.py
@@ -19,7 +19,6 @@ import warnings
 os.environ["RAPIDS_NO_INITIALIZE"] = "1"
 
 from nemo_curator import __version__
-
 from nemo_curator.classifiers import FineWebEduClassifier
 from nemo_curator.datasets import DocumentDataset
 
