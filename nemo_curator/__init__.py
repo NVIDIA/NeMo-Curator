fe5522edc73dc1f35490b436be1baf0c5fecd9ce
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2024-11-12 14:48:48 -0800

    Update __init__.py
    Signed-off-by: Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>

--- nemo_curator/__init__.py
+++ nemo_curator/__init__.py
@@ -15,6 +15,19 @@
 import dask
 
 from .modules import *
+from .package_info import (
+    __contact_emails__,
+    __contact_names__,
+    __description__,
+    __download_url__,
+    __homepage__,
+    __keywords__,
+    __license__,
+    __package_name__,
+    __repository_url__,
+    __shortversion__,
+    __version__,
+)
 from .services import (
     AsyncLLMClient,
     AsyncOpenAIClient,
