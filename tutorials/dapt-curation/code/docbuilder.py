2e265ac3175de198186bda51269bc338e347700e
ggcr <ggcristian@icloud.com>
2024-12-06 08:00:40 +0100

    Added LookUp error handling during encoding detection.

--- tutorials/dapt-curation/code/docbuilder.py
+++ tutorials/dapt-curation/code/docbuilder.py
@@ -283,7 +283,7 @@ class GitHubIterator(DocumentIterator):
 
             try:
                 content = content.decode(encoding)
-            except UnicodeDecodeError:
+            except (UnicodeDecodeError, LookupError):
                 # If the file cannot be decoded, return None
                 return None
 
