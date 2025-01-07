4f25a9148941e966104390b1322003695a77663a
oliver könig <okoenig@nvidia.com>
2025-01-07 16:11:52 +0100

    chore: Update package_info.py
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- nemo_curator/package_info.py
+++ nemo_curator/package_info.py
@@ -16,8 +16,8 @@
 MAJOR = 0
 MINOR = 6
 PATCH = 0
-PRE_RELEASE = "rc2"
-DEV = "dev1"
+PRE_RELEASE = ""
+DEV = ""
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE, DEV)
