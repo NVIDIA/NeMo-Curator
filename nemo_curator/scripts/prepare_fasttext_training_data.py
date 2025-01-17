2b1fa1157f1139c0a65f56cd5dd00f7b2e5fb5da
Sarah Yurick <sarahyurick@gmail.com>
2025-01-17 13:55:04 -0800

    input_json_field and input-json-field
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- nemo_curator/scripts/prepare_fasttext_training_data.py
+++ nemo_curator/scripts/prepare_fasttext_training_data.py
@@ -35,7 +35,7 @@ def main(args):
     files = list(get_all_files_paths_under(args.input_data_dir))
     raw_data = read_data(files, file_type="jsonl", backend="pandas")
     dataset = DocumentDataset(raw_data)
-    text_field = args.input_json_field
+    text_field = args.text_field
 
     # fastText requires each document to be prepended with a special label for training
     preprocessing = Modify(FastTextLabelModifier(args.label), text_field=text_field)
@@ -80,7 +80,7 @@ with FastText.
     )
     argumentHelper.add_distributed_args()
     parser.add_argument(
-        "--input-json-field",
+        "--text-field",
         type=str,
         default="text",
         help="The input field within each JSON object on which the filter will "
