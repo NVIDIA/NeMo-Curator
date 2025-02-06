e41c5fab07c15223888557de89780799ee2eb19d
Praateek Mahajan <praateekmahajan@users.noreply.github.com>
2025-02-06 11:41:00 -0800

    Update nemo_curator/modules/exact_dedup.py
    Co-authored-by: Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
    Signed-off-by: Praateek Mahajan <praateekmahajan@users.noreply.github.com>

--- nemo_curator/modules/exact_dedup.py
+++ nemo_curator/modules/exact_dedup.py
@@ -71,7 +71,7 @@ class ExactDuplicates:
         self.text_field = text_field
         self.perform_removal = perform_removal
         if not self.perform_removal:
-            warnings.warn("In future release (0.8.0) by default will be True.")
+            warnings.warn("In future releases (starting with 0.8.0) the default will be True.")
         if self.perform_removal and cache_dir is None:
             warnings.warn("cache_dir is recommended to remove duplicates.")
         if cache_dir is None and profile_dir is not None:
