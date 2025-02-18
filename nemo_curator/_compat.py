a4d76f66cad818d0fb08099098decdcfcc2dbe9c
Praateek <praateekm@gmail.com>
2025-02-18 12:12:13 -0800

    change to 25.2.0
    Signed-off-by: Praateek <praateekm@gmail.com>

--- nemo_curator/_compat.py
+++ nemo_curator/_compat.py
@@ -45,7 +45,7 @@ DASK_SHUFFLE_METHOD_ARG = _dask_version > parse_version("2024.1.0")
 DASK_P2P_ERROR = _dask_version < parse_version("2023.10.0")
 DASK_SHUFFLE_CAST_DTYPE = _dask_version > parse_version("2023.12.0")
 DASK_CUDF_PARQUET_READ_INCONSISTENT_SCHEMA = _dask_cudf_version > parse_version(
-    "2025.02"
+    "25.2.0"
 )
 
 # Query-planning check (and cache)
