78dc1da1bf9858a441e9e340855e02ab0a50e0a2
Praateek <praateekm@gmail.com>
2025-04-01 14:24:49 -0700

    ..
    Signed-off-by: Praateek <praateekm@gmail.com>

--- nemo_curator/modules/base.py
+++ nemo_curator/modules/base.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import warnings
 from abc import ABC, abstractmethod
 from typing import Literal, Optional, Union
