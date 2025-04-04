3f0bac502fb7518faa52ed97d017ea5010404394
Vibhu Jawa <vjawa@nvidia.com>
2025-04-04 14:19:15 -0700

    [FIX] Fix Kmeans Cancelled Error (#625)
    * Semdedup kmeans bug
    
    Signed-off-by: Vibhu Jawa <vjawa@nvidia.com>
    
    * Add link to issue
    
    Signed-off-by: Vibhu Jawa <vjawa@nvidia.com>
    
    ---------
    
    Signed-off-by: Vibhu Jawa <vjawa@nvidia.com>

--- nemo_curator/modules/semantic_dedup/clusteringmodel.py
+++ nemo_curator/modules/semantic_dedup/clusteringmodel.py
@@ -194,7 +194,10 @@ class ClusteringModel:
             )
             np.save(kmeans_centroids_file, centroids)
             self.logger.info("Saving centroids complete")
-            del kmeans, cupy_normalized_darr, centroids
+            # Deleting kmeans triggers a future cancelled error in dask
+            # See issue:https://github.com/NVIDIA/NeMo-Curator/issues/624
+            # del kmeans
+            del centroids, cupy_normalized_darr
 
             # Save embeddings by nearest center to a file
             clustering_output_dir = os.path.join(
