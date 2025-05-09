761814fbc8e66b77d84884056686cb63872cdc56
Charlie Truong <chtruong@nvidia.com>
2025-05-08 19:56:56 -0500

    Publish 0.8.0
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- nemo_curator/package_info.py
+++ nemo_curator/package_info.py
@@ -16,8 +16,8 @@
 MAJOR = 0
 MINOR = 8
 PATCH = 0
-PRE_RELEASE = 'rc4'
-DEV = 'dev0'
+PRE_RELEASE = ''
+DEV = ''
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE, DEV)
